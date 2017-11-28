8a3ea11279549496791fb336cea72c098e77e48a
xaositectt <tamas.varga.420@gmail.com>
2017-11-28 16:45:43 +0100

    finished all the day2 exercises

--- week-02/day-2/exercise_17/src/SwapElements.java
+++ week-02/day-2/exercise_17/src/SwapElements.java
@@ -1,2 +1,23 @@
+import java.util.Arrays;
+
 public class SwapElements {
+
+    // - Create an array variable named `abc`
+    //   with the following content: `["first", "second", "third"]`
+    // - Swap the first and the third element of `abc`
+
+    public static void main(String[] args) {
+
+        String[] abc = {"first", "second", "third"};
+
+        String element1 = abc[0];
+        String element2 = abc[2];
+
+        abc [0]=element2;
+        abc[2]= element1;
+
+        System.out.println(Arrays.toString(abc));
+
+
+    }
 }
---
+++ week-02/day-2/exercise_19/src/ReverseElements.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class ReverseElements {
+
+    // - Create an array variable named `aj`
+    //   with the following content: `[3, 4, 5, 6, 7]`
+    // - Reverse the order of the elements in `aj`
+    // - Print the elements of the reversed `aj`
+
+    public static void main(String[] args) {
+
+        int[] aj = {3, 4, 5, 6, 7};
+
+        for (int k = 0; k < aj.length / 2; k++) {
+
+            int store = aj[aj.length - k - 1];
+
+
+            aj[aj.length - k - 1] = aj[k];
+            aj[k] = store;
+        }
+
+
+        System.out.println(Arrays.toString(aj));
+    }
+}
