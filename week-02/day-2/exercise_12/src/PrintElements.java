aafd3f8d3a1f23a5a76b9a24177e7f51e9b2b7ec
xaositectt <tamas.varga.420@gmail.com>
2017-11-28 16:24:43 +0100

    t

---
+++ week-02/day-2/exercise_12/src/PrintElements.java
@@ -0,0 +1,18 @@
+public class PrintElements {
+    // - Create an array variable named `af`
+    //   with the following content: `[4, 5, 6, 7]`
+    // - Print all the elements of `af`
+
+    public static void main(String[] args) {
+
+        int[] af = {4, 5, 6, 7};
+
+        for (int i=0; i < af.length; i++)
+        {
+            System.out.println(af[i]);
+
+        }
+
+    }
+
+}
---
+++ week-02/day-2/exercise_14/src/DoubleItems.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class DoubleItems {
+
+    // - Create an array variable named `ag`
+    //   with the following content: `[3, 4, 5, 6, 7]`
+    // - Double all the values in the array
+
+    public static void main(String[] args) {
+
+        int[] ag = {3, 4, 5, 6, 7};
+
+        for (int i=0; i < ag.length; i++)
+        {
+           ag[i]=ag[i]*2;
+
+        }
+
+        System.out.println(Arrays.toString(ag));
+
+    }
+
+
+}
