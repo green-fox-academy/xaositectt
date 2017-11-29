ed49e539c3d3780304616a3796777d8e9cc51bbf
xaositectt <tamas.varga.420@gmail.com>
2017-11-29 10:30:39 +0100

    finished first two exercises of wednesday

---
+++ week-02/day-3/exercise_2/src/Strings_2.java
@@ -0,0 +1,26 @@
+public class Strings_2 {
+
+    public static void main(String[] args) {
+
+        String url = "https//www.reddit.com/r/nevertellmethebots";
+
+        // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+        // Also, the URL is missing a crutial component, find out what it is and insert it too!
+        // Try to solve it more than once using different String functions!
+
+        //url = url.replace("bots", "odds");
+
+        //url = url.replace("https", "https:");
+
+        String url1 = "https:";
+        String url2 = "odds";
+
+        url = url.substring(5, 38);
+
+        url= url1.concat(url);
+        url=url.concat(url2);
+
+        System.out.println(url);
+
+    }
+}
