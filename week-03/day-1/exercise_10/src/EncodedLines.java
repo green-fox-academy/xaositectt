baec89c7c4e16b70675c5248fbee0a474eed6a1a
xaositectt <tamas.varga.420@gmail.com>
2017-12-04 17:50:01 +0100

    finished all exercises up to 10

--- week-03/day-1/exercise_10/src/EncodedLines.java
+++ week-03/day-1/exercise_10/src/EncodedLines.java
@@ -5,11 +5,9 @@ import java.io.*;
 public class EncodedLines {
   public static void main(String[] args) {
     // Create a method that decrypts the duplicated-chars.txt
-
     Path filePath = Paths.get("src/encoded-lines.txt");
     decrypt(filePath);
   }
-
   private static void decrypt(Path mypath) {
     try {
       List<String> crypticList = Files.readAllLines(mypath);
@@ -23,7 +21,6 @@ public class EncodedLines {
           String next = String.valueOf((char)(charValue-1));
           currentWord=currentWord+next;
         }
-
         currentString=currentString+currentWord +" ";
         decryptedList.add(currentString);
       }
--- week-03/day-1/try/src/try_class.java
+++ week-03/day-1/try/src/try_class.java
@@ -1,8 +1,8 @@
 public class try_class {
   public static void main(String[] args) {
-    String value = "U";
+    String value = " ";
     int charValue = value.charAt(0);
-    String next = String.valueOf( (char) (charValue + 1));
+    String next = String.valueOf( (char) (charValue -1));
     System.out.println(next);
     }
   }
